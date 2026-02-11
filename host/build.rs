3ab9cc5ef587118cf49da48d5b6e3eeb5edde0da
RogerTaule <roger.taulee@gmail.com>
2026-02-11 17:38:48 +0000

    More changes

--- host/build.rs
+++ host/build.rs
@@ -1,13 +1,13 @@
 use std::path::PathBuf;
-use zisk_common::io::{ZiskIO, ZiskStdin};
+use zisk_sdk::{ZiskIO, ZiskStdin, build_program};
 
 fn main() {
-    zisk_build::build_program("../guest");
+    build_program("../guest");
     let n = 1000u32;
     let stdin_save = ZiskStdin::new();
     stdin_save.write(&n);
     // Check if path exists, if not write
-    let path = PathBuf::from("tmp/input.bin");
+    let path = PathBuf::from("tmp/verify_constraints_input.bin");
     if let Some(parent) = path.parent() {
         std::fs::create_dir_all(parent).unwrap();
     }

e6e41e6b70e0f08b99ba4fe3658dc1ae2bb51b58
agnusmor <100322135+agnusmor@users.noreply.github.com>
2026-03-12 14:50:04 +0100

    Remove unused ZiskIO import from main.rs

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use zisk_sdk::{ZiskStdin, ZiskIO, ElfBinary, ProofOpts, ProverClient, include_elf};
+use zisk_sdk::{ZiskStdin, ElfBinary, ProofOpts, ProverClient, include_elf};
 
 pub const ELF: ElfBinary = include_elf!("guest");
 
